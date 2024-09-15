dcfa99c77def54120ec6a1d43351002452b6b08b
Napat Rungruangbangchan <Napat.joe@gmail.com>
2024-09-15 15:33:44 +0700

    Create slsa-goreleaser.yml
    Ref https://github.com/slsa-framework/slsa-github-generator/blob/main/internal/builders/go/README.md

--- main.go
+++ main.go
@@ -1,6 +1,17 @@
 package main
 
+import "log"
+
+var (
+	Version    string
+	Commit     string
+	CommitDate string
+	TreeState  string
+)
+
 func main() {
+	log.Printf("Version: %s, Commit: %s, Date: %s, State: %s\n", Version, Commit, CommitDate, TreeState)
+
 	server := NewAPIServer(":8080")
 	server.Start()
 }
